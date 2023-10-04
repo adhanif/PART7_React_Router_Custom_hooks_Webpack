3b1d9b90f3cfe72d49749e74e4461193a450302e
ad.hanif <ad.hanif@outlook.com>
2023-10-04 13:18:38 +0200

    7.1: routed anecdotes, step1 completed

--- routed-anecdotes/src/App.jsx
+++ routed-anecdotes/src/App.jsx
@@ -1,9 +1,42 @@
 import "./App.css";
+import { Routes, Route, NavLink, Link } from "react-router-dom";
+import { useState } from "react";
+import Footer from "./components/Footer";
+import Menu from "./components/Menu";
+import AnecdoteList from "./components/AnecdoteList";
+import About from "./components/About";
+import CreateNew from "./components/CreateNew";
 
 function App() {
- 
+  const [anecdotes, setAnecdotes] = useState([
+    {
+      content: "If it hurts, do it more often",
+      author: "Jez Humble",
+      info: "https://martinfowler.com/bliki/FrequencyReducesDifficulty.html",
+      votes: 0,
+      id: 1,
+    },
+    {
+      content: "Premature optimization is the root of all evil",
+      author: "Donald Knuth",
+      info: "http://wiki.c2.com/?PrematureOptimization",
+      votes: 0,
+      id: 2,
+    },
+  ]);
 
-  return <></>;
+  return (
+    <>
+      <Menu />
+      <Routes>
+        <Route path="/" element={<AnecdoteList anecdotes={anecdotes} />} />
+        <Route path="/create" element={<CreateNew />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+
+      <Footer />
+    </>
+  );
 }
 
 export default App;
